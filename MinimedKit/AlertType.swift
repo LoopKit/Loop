ba43b5a9844673646fa8c2f8129181d5a356c7f6
Nathan Racklyeft <loudnate@gmail.com>
2015-09-15 20:47:13 -0700

    Adding additional alert types

--- MinimedKit/AlertType.swift
+++ MinimedKit/AlertType.swift
@@ -7,9 +7,16 @@
 //
 
 public enum AlertType: UInt8 {
-    case NoDelivery     = 0x04
-    case MaxHourlyBolus = 0x33
-    case HighPredicted  = 0x72
-    case LowPredicted   = 0x73
-    case MeterBGNow     = 0x68
+    case NoDelivery         = 0x04
+    case MaxHourlyBolus     = 0x33
+    case HighGlucose        = 0x65
+    case LowGlucose         = 0x66
+    case MeterBGNow         = 0x68
+    case MeterBGSoon        = 0x69
+    case CalibrationError   = 0x6a
+    case SensorEnd          = 0x6b
+    case WeakSignal         = 0x70
+    case LostSensor         = 0x71
+    case HighPredicted      = 0x72
+    case LowPredicted       = 0x73
 }
