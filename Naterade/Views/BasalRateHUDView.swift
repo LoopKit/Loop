1d5e3643aaffde56c3370e2a7e4ad8aa8b8299fb
Nathan Racklyeft <loudnate@gmail.com>
2016-05-17 21:15:55 -0700

    Show the net basal rate in the HUD view

--- Naterade/Views/BasalRateHUDView.swift
+++ Naterade/Views/BasalRateHUDView.swift
@@ -12,11 +12,29 @@ class BasalRateHUDView: HUDView {
 
     @IBOutlet private var basalStateView: BasalStateView!
 
+    @IBOutlet var basalRateLabel: UILabel! {
+        didSet {
+            basalRateLabel?.text = "— U"
+            basalRateLabel?.textColor = UIColor.doseTintColor
+        }
+    }
+
     func setNetBasalRate(rate: Double, percent: Double, atDate date: NSDate) {
         caption?.text = timeFormatter.stringFromDate(date)
-
+        basalRateLabel?.text = "\(decimalFormatter.stringFromNumber(rate) ?? "–") U"
         basalStateView.netBasalPercent = percent
     }
+
+    private lazy var decimalFormatter: NSNumberFormatter = {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .DecimalStyle
+        formatter.minimumFractionDigits = 1
+        formatter.minimumIntegerDigits = 1
+        formatter.positiveFormat = "+0.0##"
+        formatter.negativeFormat = "-0.0##"
+
+        return formatter
+    }()
 
     private lazy var timeFormatter: NSDateFormatter = {
         let formatter = NSDateFormatter()
