673562609e148c0ce4b824089beaeaba51e314fc
Jeremy Lucas <jlucasvt@gmail.com>
2016-07-16 15:05:31 -0400

    x22 Pump Reservoir Display
    Added switch for reservoir level display or 722 and 522 pumps.

--- Loop/View Controllers/StatusTableViewController.swift
+++ Loop/View Controllers/StatusTableViewController.swift
@@ -195,7 +195,16 @@ class StatusTableViewController: UITableViewController, UIGestureRecognizerDeleg
             }
 
             reservoirVolume = dataManager.latestReservoirValue?.unitVolume
-
+			
+			// Remaining Volume HUD for x22 pumps
+			if reservoirVolume > 0 {
+				switch NSUserDefaults.standardUserDefaults().pumpModelNumber! {
+				case "722": reservoirLevel = Double(reservoirVolume! / 300) / 100  // 722 Reservior Vol = 300u
+				case "522": reservoirLevel = Double(reservoirVolume! / 176) / 100  // 522 Reservoir Vol= 176u
+				default: reservoirLevel = 0
+				}
+			}
+			
             if let status = dataManager.latestPumpStatus {
                 reservoirLevel = Double(status.reservoirRemainingPercent) / 100
                 batteryLevel = Double(status.batteryRemainingPercent) / 100
