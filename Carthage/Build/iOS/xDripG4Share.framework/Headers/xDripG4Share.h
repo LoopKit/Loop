01a765a8ce9ee119bf5c1829ce46f94fbcecec84
Mark Wilson <mark@warkmilson.com>
2016-07-24 19:51:13 -0700

    Commit xDripG4Share.framework

---
+++ Carthage/Build/iOS/xDripG4Share.framework/Headers/xDripG4Share.h
@@ -0,0 +1,19 @@
+//
+//  xDripG4Share.h
+//  xDripG4Share
+//
+//  Created by Mark on 7/21/16.
+//  Copyright © 2016 Mark Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for xDripG4Share.
+FOUNDATION_EXPORT double xDripG4ShareVersionNumber;
+
+//! Project version string for xDripG4Share.
+FOUNDATION_EXPORT const unsigned char xDripG4ShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xDripG4Share/PublicHeader.h>
+
+
