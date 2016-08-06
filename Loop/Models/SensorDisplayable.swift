332590e29fc32bcb8217157130e80b91c26c30d7
Nathan Racklyeft <loudnate@gmail.com>
2016-08-05 22:49:18 -0700

    Fixing the watch description

---
+++ Loop/Models/SensorDisplayable.swift
@@ -0,0 +1,19 @@
+//
+//  SensorDisplayable.swift
+//  Loop
+//
+//  Created by Nate Racklyeft on 8/2/16.
+//  Copyright © 2016 Nathan Racklyeft. All rights reserved.
+//
+
+
+protocol SensorDisplayable {
+    // Describes the state of the sensor in the current localization
+    var stateDescription: String { get }
+
+    /// Enumerates the trend of the sensor values
+    var trendType: GlucoseTrend? { get }
+
+    // Describes the trend of the sensor values in the current localization
+    var trendDescription: String { get }
+}
