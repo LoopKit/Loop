cc3ead23aa0c627ebdd336377b11edaa13c537f4
Pete Schwamb <pete@schwamb.net>
2016-09-03 15:56:16 -0500

    Use PumpModel.hasMySentry over larger where it makes sense

--- Loop/Managers/DeviceDataManager.swift
+++ Loop/Managers/DeviceDataManager.swift
@@ -665,7 +665,7 @@ final class DeviceDataManager: CarbStoreDelegate, DoseStoreDelegate, Transmitter
                 }
             }
         case "pumpModel"?:
-            if let sentrySupported = pumpState?.pumpModel?.larger where !sentrySupported {
+            if let sentrySupported = pumpState?.pumpModel?.hasMySentry where !sentrySupported {
                 rileyLinkManager.idleListeningEnabled = false
             }
 
@@ -899,7 +899,7 @@ final class DeviceDataManager: CarbStoreDelegate, DoseStoreDelegate, Transmitter
                 if let model = PumpModel(rawValue: pumpModelNumber) {
                     pumpState.pumpModel = model
 
-                    idleListeningEnabled = model.larger
+                    idleListeningEnabled = model.hasMySentry
                 }
             }
 
