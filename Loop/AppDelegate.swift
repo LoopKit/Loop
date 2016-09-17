6c3c9bd9eb9fc55da5903f3f59d570ef0cb7803f
Nathan Racklyeft <loudnate@gmail.com>
2016-09-16 20:36:42 -0700

    Migrate to UserNotifications.framework (WIP)

--- Loop/AppDelegate.swift
+++ Loop/AppDelegate.swift
@@ -99,3 +99,14 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         completionHandler([.badge, .sound, .alert])
     }
 }
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.badge, .sound, .alert])
+    }
+}
