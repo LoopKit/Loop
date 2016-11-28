d200128b36204e69ce2dc39f0ddd4b171063935b
Bharat Mediratta <bharat@altschool.com>
2016-11-27 16:12:59 -0800

    Remove boilerplate comments

--- Loop Status Extension/StatusViewController.swift
+++ Loop Status Extension/StatusViewController.swift
@@ -24,23 +24,13 @@ class StatusViewController: UIViewController, NCWidgetProviding {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
-        
         if let context = UserDefaults.shared()?.statusExtensionContext {
             if let glucose = context.latestGlucose {
                 glucoseHUD.set(glucose.latest, for: HKUnit.milligramsPerDeciliterUnit(), from: glucose.sensor)
