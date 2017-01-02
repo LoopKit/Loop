99bf3f8458577ae24ba923edc705078b5a53903a
Bharat Mediratta <bharat@altschool.com>
2017-01-01 19:49:34 -0800

    Disable animation on first render, since we'd be animating from a default value so the animation is deceptive.

--- LoopUI/Views/LoopStateView.swift
+++ LoopUI/Views/LoopStateView.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 public final class LoopStateView: UIView {
+    var firstDataUpdate = true
+    
     enum Freshness {
         case fresh
         case aging
@@ -112,7 +114,7 @@ public final class LoopStateView: UIView {
 
                     let group = CAAnimationGroup()
                     group.animations = [path, width]
-                    group.duration = 1
+                    group.duration = firstDataUpdate ? 0 : 1
                     group.repeatCount = HUGE
                     group.autoreverses = true
                     group.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
@@ -122,6 +124,7 @@ public final class LoopStateView: UIView {
                     shapeLayer.removeAnimation(forKey: type(of: self).AnimationKey)
                 }
             }
+            firstDataUpdate = false
         }
     }
 }
