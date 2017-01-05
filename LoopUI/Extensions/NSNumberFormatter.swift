9d9890b002053aed4a50aef678fc56128452fa86
Bharat Mediratta <bharat@altschool.com>
2017-01-04 21:20:25 -0800

    Enforce a fixed number of fractional digits per unit type. (#356)
    * Enforce a fixed number of fractional digits per unit type.
    
    At all times, we prefer:
      0 fractional digits for mg/dL
      1 fractional digit
    
    eliminate the use of significant digits and use fixed a number of
    fractional digits everywhere.
    
    This is in service of:
      https://github.com/LoopKit/Loop/issues/324
    
    * Pass the glucose NumberFormatter directly into the chart
    
    This lets us back out the changes to PredictionTableViewController
    since now the ChartAxisDoubleUnit uses the glucose formatter to
    represent its value instead of hardcoding an integer formatter.

--- LoopUI/Extensions/NSNumberFormatter.swift
+++ LoopUI/Extensions/NSNumberFormatter.swift
@@ -14,10 +14,8 @@ extension NumberFormatter {
     public static func glucoseFormatter(for unit: HKUnit) -> NumberFormatter {
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
-        numberFormatter.minimumFractionDigits = unit.preferredMinimumFractionDigits
-        numberFormatter.maximumSignificantDigits = 3
-        numberFormatter.usesSignificantDigits = true
-
+        numberFormatter.minimumFractionDigits = unit.preferredFractionDigits
+        numberFormatter.maximumFractionDigits = unit.preferredFractionDigits
         return numberFormatter
     }
 }
