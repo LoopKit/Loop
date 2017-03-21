9b74b6ce7f2dfa8b883a8a1b51b2a3cc69f4b265
Bharat Mediratta <bharat@altschool.com>
2017-03-21 14:22:30 -0700

    Combine the two StatusChartsManager extensions in Loop

--- Loop/Managers/StatusChartsManager+LoopKit.swift
+++ Loop/Managers/StatusChartsManager+LoopKit.swift
@@ -14,8 +14,45 @@ import InsulinKit
 import LoopKit
 import SwiftCharts
 
+private var glucoseTargetRangeScheduleAssociationKey: UInt8 = 0
 
 extension StatusChartsManager {
+
+    var glucoseTargetRangeSchedule: GlucoseRangeSchedule? {
+        get {
+            return objc_getAssociatedObject(self, &glucoseTargetRangeScheduleAssociationKey) as? GlucoseRangeSchedule
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self,
+                                     &glucoseTargetRangeScheduleAssociationKey,
+                                     newValue,
+                                     objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+            targetGlucosePoints = []
+        }
+    }
+
+    /// Runs any necessary steps before rendering charts
+    func prerender() {
+        if xAxisValues == nil {
+            generateXAxisValues()
+        }
+
+        if  let xAxisValues = xAxisValues, xAxisValues.count > 1,
+            targetGlucosePoints.count == 0,
+            let targets = glucoseTargetRangeSchedule
+        {
+            targetGlucosePoints = ChartPoint.pointsForGlucoseRangeSchedule(targets, xAxisValues: xAxisValues)
+
+            if let override = targets.temporaryOverride {
+                targetOverridePoints = ChartPoint.pointsForGlucoseRangeScheduleOverride(override, xAxisValues: xAxisValues)
+
+                targetOverrideDurationPoints = ChartPoint.pointsForGlucoseRangeScheduleOverrideDuration(override, xAxisValues: xAxisValues)
+            } else {
+                targetOverridePoints = []
+                targetOverrideDurationPoints = []
+            }
+        }
+    }    
 
     private var dateFormatter: DateFormatter {
         let timeFormatter = DateFormatter()
