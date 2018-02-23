c4f53bcf36c5e6ba1a2506a5a5a4a87cdf8ad00a
Kdisimone <kdisimone@mac.com>
2018-02-23 14:06:35 -0800

    removed TODO per @trixing's review of PR

--- Loop/View Controllers/SettingsTableViewController.swift
+++ Loop/View Controllers/SettingsTableViewController.swift
@@ -69,7 +69,7 @@ final class SettingsTableViewController: UITableViewController, DailyValueSchedu
         super.viewDidDisappear(animated)
 
         dataManager.rileyLinkManager.setDeviceScanningEnabled(false)
-        // TODO: Do not upload every time we exit settings.  Perhaps have a check for change?
+    
         if let uploader = dataManager.remoteDataManager.nightscoutService.uploader {
             UserDefaults.standard.uploadProfile(uploader: uploader)
         }
