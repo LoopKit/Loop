64b23c9ea9ef4bb72443596c1ceda1ca3143d0d5
Pete Schwamb <pete@schwamb.net>
2018-09-18 11:24:22 -0500

    Shorcuts: Add Carbs

---
+++ Loop/View Controllers/RootNavigationController.swift
@@ -0,0 +1,53 @@
+//
+//  RootNavigationController.swift
+//  Loop
+//
+//  Copyright © 2018 LoopKit Authors. All rights reserved.
+//
+
+import UIKit
+import LoopKit
+import LoopKitUI
+
+/// The root view controller in Loop
+class RootNavigationController: UINavigationController {
+
+    /// Its root view controller is always StatusTableViewController after loading
+    var rootViewController: StatusTableViewController! {
+        return viewControllers.first as? StatusTableViewController
+    }
+
+    override func restoreUserActivityState(_ activity: NSUserActivity) {
+        switch activity.activityType {
+        case NSUserActivity.viewLoopStatusActivityType:
+            if presentedViewController != nil {
+                dismiss(animated: false, completion: nil)
+            }
+
+            if viewControllers.count > 1 {
+                popToRootViewController(animated: false)
+            }
+        case NSUserActivity.newCarbEntryActivityType:
+            if let navVC = presentedViewController as? UINavigationController {
+                if let carbVC = navVC.topViewController as? CarbEntryEditViewController {
+                    carbVC.restoreUserActivityState(activity)
+                    return
+                } else {
+                    dismiss(animated: false, completion: nil)
+                }
+            }
+
+            if let carbVC = topViewController as? CarbAbsorptionViewController {
+                carbVC.restoreUserActivityState(activity)
+                return
+            } else if viewControllers.count > 1 {
+                popToRootViewController(animated: false)
+            }
+
+            fallthrough
+        default:
+            rootViewController.restoreUserActivityState(activity)
+        }
+    }
+
+}
--- WatchApp Extension/Controllers/StatusInterfaceController.swift
+++ WatchApp Extension/Controllers/StatusInterfaceController.swift
@@ -43,6 +43,13 @@ final class StatusInterfaceController: WKInterfaceController, ContextUpdatable {
         super.willActivate()
 
         updateLoopHUD()
+
+        let userActivity = NSUserActivity.forViewLoopStatus()
+        if #available(watchOSApplicationExtension 5.0, *) {
+            update(userActivity)
+        } else {
+            updateUserActivity(userActivity.activityType, userInfo: userActivity.userInfo, webpageURL: nil)
+        }
     }
 
     private func updateLoopHUD() {
