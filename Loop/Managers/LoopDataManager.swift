f0b9429340d45765a65bfa820e9c35da5efc7cb9
Pete Schwamb <pete@schwamb.net>
2019-04-04 09:49:33 -0500

    Display recently-entered carbs in Watch app (#908)
    * Display recently-entered carbs in Watch app
    
    - Refactors the watch interface controller to use a WKInterfaceTable
    - Present a list of recently logged carbs from HealthKit on tap of the COB cell
    - Proactively save new carb entries to HealthKit after successfully logging from the watch app
    - Add a new cell to display remaining reservoir volume
    
    * Have travis update brew before building

--- Loop/Managers/LoopDataManager.swift
+++ Loop/Managers/LoopDataManager.swift
@@ -62,11 +62,7 @@ final class LoopDataManager {
         carbStore = CarbStore(
             healthStore: healthStore,
             cacheStore: cacheStore,
-            defaultAbsorptionTimes: (
-                fast: TimeInterval(hours: 2),
-                medium: TimeInterval(hours: 3),
-                slow: TimeInterval(hours: 4)
-            ),
+            defaultAbsorptionTimes: LoopSettings.defaultCarbAbsorptionTimes,
             carbRatioSchedule: carbRatioSchedule,
             insulinSensitivitySchedule: insulinSensitivitySchedule
         )
