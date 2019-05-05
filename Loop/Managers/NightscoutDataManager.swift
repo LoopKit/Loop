3cec2f3b3a25034977bea98ee6243d01060c45dd
Jason Calabrese <jason@cbrese.com>
2019-05-04 21:56:15 -0700

    only upload the reservoir units if they're current

--- Loop/Managers/NightscoutDataManager.swift
+++ Loop/Managers/NightscoutDataManager.swift
@@ -152,6 +152,13 @@ final class NightscoutDataManager {
                 bolusing = false
             }
             
+            let currentReservoirUnits: Double?
+            if let lastReservoirValue = deviceManager.loopManager.doseStore.lastReservoirValue, lastReservoirValue.startDate > Date().addingTimeInterval(.minutes(-15)) {
+                currentReservoirUnits = lastReservoirValue.unitVolume
+            } else {
+                currentReservoirUnits = nil
+            }
+
             pumpStatus = NightscoutUploadKit.PumpStatus(
                 clock: Date(),
                 pumpID: pumpManagerStatus.device.localIdentifier ?? "Unknown",
@@ -161,7 +168,7 @@ final class NightscoutDataManager {
                 battery: battery,
                 suspended: pumpManagerStatus.basalDeliveryState == .suspended,
                 bolusing: bolusing,
-                reservoir: deviceManager.loopManager.doseStore.lastReservoirValue?.unitVolume,
+                reservoir: currentReservoirUnits,
                 secondsFromGMT: pumpManagerStatus.timeZone.secondsFromGMT())
         } else {
             pumpStatus = nil
