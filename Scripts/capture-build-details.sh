d6105aa7b1ae932fb8420cf7fcb0b5da9c059225
Pete Schwamb <pete@schwamb.net>
2019-09-14 20:40:56 -0500

    Fix build details for non-git builds

--- Scripts/capture-build-details.sh
+++ Scripts/capture-build-details.sh
@@ -8,12 +8,15 @@
 echo "Gathering build details in ${SRCROOT}"
 cd ${SRCROOT}
 
-rev=$(git rev-parse HEAD)
-branch=$(git branch | grep \* | cut -d ' ' -f2-)
 plist="${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"
 
-plutil -replace com-loopkit-Loop-git-revision -string ${rev} "${plist}"
-plutil -replace com-loopkit-Loop-git-branch -string "${branch}" "${plist}"
+if [ -e .git ]; then
+  rev=$(git rev-parse HEAD)
+  plutil -replace com-loopkit-Loop-git-revision -string ${rev} "${plist}"
+  branch=$(git branch | grep \* | cut -d ' ' -f2-)
+  plutil -replace com-loopkit-Loop-git-branch -string "${branch}" "${plist}"
+fi;
 plutil -replace com-loopkit-Loop-srcroot -string ${SRCROOT} "${plist}"
 plutil -replace com-loopkit-Loop-build-date -string "$(date)" "${plist}"
 plutil -replace com-loopkit-Loop-xcode-version -string "${XCODE_PRODUCT_BUILD_VERSION}" "${plist}"
+
