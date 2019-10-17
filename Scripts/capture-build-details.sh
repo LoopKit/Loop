255cf8e8a931103265a2f4eece55d9d49bfd5e07
Artiom Kenibasov <artiom@gmail.com>
2019-10-18 01:18:04 +0300

    Fixed build from directory containing spaces

--- Scripts/capture-build-details.sh
+++ Scripts/capture-build-details.sh
@@ -16,7 +16,7 @@ if [ -e .git ]; then
   branch=$(git branch | grep \* | cut -d ' ' -f2-)
   plutil -replace com-loopkit-Loop-git-branch -string "${branch}" "${plist}"
 fi;
-plutil -replace com-loopkit-Loop-srcroot -string ${SRCROOT} "${plist}"
+plutil -replace com-loopkit-Loop-srcroot -string "${SRCROOT}" "${plist}"
 plutil -replace com-loopkit-Loop-build-date -string "$(date)" "${plist}"
 plutil -replace com-loopkit-Loop-xcode-version -string "${XCODE_PRODUCT_BUILD_VERSION}" "${plist}"
 
