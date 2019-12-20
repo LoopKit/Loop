9888b57a88c7451b20f7dae92a0173ce63701251
Ivan Valkou <zloyfey@gmail.com>
2019-12-20 12:59:18 +0300

    Remove Settings.bundle

--- Loop/Managers/LoopDataManager.swift
+++ Loop/Managers/LoopDataManager.swift
@@ -71,8 +71,7 @@ final class LoopDataManager {
             defaultAbsorptionTimes: LoopSettings.defaultCarbAbsorptionTimes,
             carbRatioSchedule: carbRatioSchedule,
             insulinSensitivitySchedule: insulinSensitivitySchedule,
-            overrideHistory: overrideHistory,
-            carbAbsorptionModel: settings.carbAbsorptionModel
+            overrideHistory: overrideHistory
         )
 
         doseStore = DoseStore(
