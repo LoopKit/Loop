4b6f0c259f053c397285364f30f31428ed836c68
Daniel Mini Johansson <42831533+CaspianGeben@users.noreply.github.com>
2020-01-12 11:17:11 +0100

    Update MealBolusNightscoutTreatment.swift

--- Loop/Extensions/MealBolusNightscoutTreatment.swift
+++ Loop/Extensions/MealBolusNightscoutTreatment.swift
@@ -12,8 +12,22 @@ import NightscoutUploadKit
 import HealthKit
 
 extension MealBolusNightscoutTreatment {
+    
     public convenience init(carbEntry: StoredCarbEntry) {
         let carbGrams = carbEntry.quantity.doubleValue(for: HKUnit.gram())
-        self.init(timestamp: carbEntry.startDate, enteredBy: "loop://\(UIDevice.current.name)", id: carbEntry.externalID, carbs: lround(carbGrams), absorptionTime: carbEntry.absorptionTime, foodType: carbEntry.foodType)
+    
+       var myFoodType = carbEntry.foodType;
+        
+        if (carbEntry.absorptionTime == 7200){
+            myFoodType = "🍭";
+        } else if (carbEntry.absorptionTime == 10800){
+            myFoodType = "🌮";
+        } else if (carbEntry.absorptionTime == 14400){
+            myFoodType = "🍕";
+        }    
+       
+        self.init(timestamp: carbEntry.startDate, enteredBy: "loop://\(UIDevice.current.name)", id: carbEntry.externalID, carbs: lround(carbGrams), absorptionTime: carbEntry.absorptionTime, foodType: myFoodType // + carbEntry.foodType
+        )
+        
     }
 }
