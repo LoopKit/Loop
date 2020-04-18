c6b0599bf5368af6a03eed25115f5ed995471d66
AQ <anna.quinlan123@gmail.com>
2020-04-17 20:42:42 -0700

    Remove lastReconciliation

--- Loop/Managers/LoopDataManager.swift
+++ Loop/Managers/LoopDataManager.swift
@@ -614,10 +614,9 @@ extension LoopDataManager {
     ///   - events: The pump events to add
     ///   - completion: A closure called once upon completion
     ///   - lastReconciliation: The date that pump events were most recently reconciled against recorded pump history. Pump events are assumed to be reflective of delivery up until this point in time. If reservoir values are recorded after this time, they may be used to supplement event based delivery.
-    ///   - parameter updateLastReconciliation: Whether or not the last reconciliation time should be updated with the new value.
     ///   - error: An error explaining why the events could not be saved.
-    func addPumpEvents(_ events: [NewPumpEvent], lastReconciliation: Date?, updateLastReconciliation: Bool = true, completion: @escaping (_ error: DoseStore.DoseStoreError?) -> Void) {
-        doseStore.addPumpEvents(events, lastReconciliation: lastReconciliation, updateLastReconciliation: updateLastReconciliation) { (error) in
+    func addPumpEvents(_ events: [NewPumpEvent], lastReconciliation: Date?, completion: @escaping (_ error: DoseStore.DoseStoreError?) -> Void) {
+        doseStore.addPumpEvents(events, lastReconciliation: lastReconciliation) { (error) in
             self.dataAccessQueue.async {
                 if error == nil {
                     self.insulinEffect = nil
