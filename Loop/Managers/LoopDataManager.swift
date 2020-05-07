62d829c2892a28161e40d459437d468f375f94c4
Darin Krauss <darin@tidepool.org>
2020-05-06 17:13:53 -0700

    LOOP-884 Add Core Data storage for Loop settings (#81)
    - https://tidepool.atlassian.net/browse/LOOP-884
    - Update SettingsStore initialization
    - Store bloodGlucoseUnit as string for serialization

--- Loop/Managers/LoopDataManager.swift
+++ Loop/Managers/LoopDataManager.swift
@@ -93,7 +93,7 @@ final class LoopDataManager {
 
         glucoseStore = GlucoseStore(healthStore: healthStore, cacheStore: cacheStore, cacheLength: .hours(24))
 
-        settingsStore = SettingsStore(storeCache: UserDefaults.appGroup!)
+        settingsStore = SettingsStore(store: cacheStore, expireAfter: .hours(24))
 
         retrospectiveCorrection = settings.enabledRetrospectiveCorrectionAlgorithm
 
