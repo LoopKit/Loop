d57d291315f4b550fb2f55319235f2bb4fc70b6b
zion906 <66850809+zion906@users.noreply.github.com>
2020-07-01 18:33:57 -0400

    Update LoopDataManager.swift

--- Loop/Managers/LoopDataManager.swift
+++ Loop/Managers/LoopDataManager.swift
@@ -1186,6 +1186,14 @@ extension LoopDataManager {
         let predictedGlucoseIncludingPendingInsulin = try predictGlucose(using: settings.enabledEffects, includingPendingInsulin: true)
         self.predictedGlucoseIncludingPendingInsulin = predictedGlucoseIncludingPendingInsulin
 
+        if( settings.dosingStrategyAutomationEnabled && settings.dosingStrategyThreshold?.rawValue != nil){
+            if( glucose.quantity > HKQuantity(unit : HKUnit.milligramsPerDeciliter, doubleValue: settings.dosingStrategyThreshold!.value)){
+                settings.dosingStrategy = .automaticBolus
+            } else {
+                settings.dosingStrategy = .tempBasalOnly
+            }
+        }
+
         guard
             let maxBasal = settings.maximumBasalRatePerHour,
             let glucoseTargetRange = settings.glucoseTargetRangeScheduleApplyingOverrideIfActive,
