b56565f5e710d96eb4845c6f7382d10dab9b171e
Nathaniel Hamming <nate@hmtconsulting.org>
2020-09-30 11:53:52 -0300

    Nate/feature/LOOP-1840/carb entry time ux (#265)
    * committing initial work to come back to in the future
    
    * set new time entry min/max limits and use steppable date picker

--- Loop/View Controllers/CarbEntryViewController.swift
+++ Loop/View Controllers/CarbEntryViewController.swift
@@ -167,6 +167,7 @@ final class CarbEntryViewController: LoopChartsTableViewController, Identifiable
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 44
         tableView.register(DateAndDurationTableViewCell.nib(), forCellReuseIdentifier: DateAndDurationTableViewCell.className)
+        tableView.register(DateAndDurationSteppableTableViewCell.nib(), forCellReuseIdentifier: DateAndDurationSteppableTableViewCell.className)
 
         if originalCarbEntry != nil {
             title = NSLocalizedString("carb-entry-title-edit", value: "Edit Carb Entry", comment: "The title of the view controller to edit an existing carb entry")
@@ -341,16 +342,17 @@ final class CarbEntryViewController: LoopChartsTableViewController, Identifiable
                 
                 return cell
             case .date:
-                let cell = tableView.dequeueReusableCell(withIdentifier: DateAndDurationTableViewCell.className) as! DateAndDurationTableViewCell
-                
+                let cell = tableView.dequeueReusableCell(withIdentifier: DateAndDurationSteppableTableViewCell.className) as! DateAndDurationSteppableTableViewCell
+            
                 cell.titleLabel.text = NSLocalizedString("Date", comment: "Title of the carb entry date picker cell")
                 cell.datePicker.isEnabled = isSampleEditable
                 cell.datePicker.datePickerMode = .dateAndTime
-                cell.datePicker.maximumDate = Date(timeIntervalSinceNow: maximumDateFutureInterval)
+                cell.datePicker.maximumDate = date.addingTimeInterval(.hours(1))
+                cell.datePicker.minimumDate = date.addingTimeInterval(.hours(-12))
                 cell.datePicker.minuteInterval = 1
                 cell.date = date
                 cell.delegate = self
-                
+
                 return cell
             case .foodType:
                 if usesCustomFoodType {
@@ -559,7 +561,6 @@ extension CarbEntryViewController: TextFieldTableViewCellDelegate {
     }
 }
 
-
 extension CarbEntryViewController: DatePickerTableViewCellDelegate {
     func datePickerTableViewCellDidUpdateDate(_ cell: DatePickerTableViewCell) {
         guard let row = tableView.indexPath(for: cell)?.row else { return }
@@ -637,3 +638,5 @@ extension CarbEntryViewController: EmojiInputControllerDelegate {
 }
 
 extension DateAndDurationTableViewCell: NibLoadable {}
+
+extension DateAndDurationSteppableTableViewCell: NibLoadable {}
