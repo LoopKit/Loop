f0b6d20bdb81a3f44d853ce5212e0ffbadd079cd
Anna <anna@tidepool.org>
2020-10-17 16:56:47 -0700

    Base work to create intent target

---
+++ Loop Intent Extension/IntentHandler.swift
@@ -0,0 +1,28 @@
+//
+//  IntentHandler.swift
+//  Loop Intent Extension
+//
+//  Created by Anna Quinlan on 10/17/20.
+//  Copyright © 2020 LoopKit Authors. All rights reserved.
+//
+
+import Intents
+
+// As an example, this class is set up to handle Message intents.
+// You will want to replace this or add other intents as appropriate.
+// The intents you wish to handle must be declared in the extension's Info.plist.
+
+// You can test your example integration by saying things to Siri like:
+// "Send a message using <myApp>"
+// "<myApp> John saying hello"
+// "Search for messages in <myApp>"
+
+class IntentHandler: INExtension, INSendMessageIntentHandling, INSearchForMessagesIntentHandling, INSetMessageAttributeIntentHandling {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+}
