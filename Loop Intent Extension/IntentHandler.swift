12657bca54e2133b3b46b58e99d35734df8eee26
Anna <anna@tidepool.org>
2020-10-17 21:13:33 -0700

    Remove unneeded text + add intent build scheme

--- Loop Intent Extension/IntentHandler.swift
+++ Loop Intent Extension/IntentHandler.swift
@@ -8,17 +8,7 @@
 
 import Intents
 
-// As an example, this class is set up to handle Message intents.
-// You will want to replace this or add other intents as appropriate.
-// The intents you wish to handle must be declared in the extension's Info.plist.
-
-// You can test your example integration by saying things to Siri like:
-// "Send a message using <myApp>"
-// "<myApp> John saying hello"
-// "Search for messages in <myApp>"
-
 class IntentHandler: INExtension {
-    
     override func handler(for intent: INIntent) -> Any {
         if intent is EnableOverridePresetIntent {
             return OverrideIntentHandler()
