f90d2fdb3e5be400c51d22ca77cb5a6ab4536d33
Nathaniel Hamming <nate@hmtconsulting.org>
2021-03-18 13:55:59 -0300

    [LOOP-3122] Dimmed device lifecycle progress (#377)
    * using lifecycle progress state dimmed to color bar
    
    * using lifecycle progress state dimmed to color bar

--- LoopUI/Extensions/DeviceLifecycleProgressState.swift
+++ LoopUI/Extensions/DeviceLifecycleProgressState.swift
@@ -7,18 +7,25 @@
 //
 
 import LoopKit
+import SwiftUI
 
 extension DeviceLifecycleProgressState {
     public var color: UIColor {
         switch self {
+        case .critical:
+            return .critical
+        case .dimmed:
+            if #available(iOS 14.0, *) {
+                return UIColor(Color.secondary)
+            } else {
+                return .systemGray
+            }
         case .normalCGM:
             return .glucose
         case .normalPump:
             return .insulin
         case .warning:
             return .warning
-        case .critical:
-            return .critical
         }
     }
 }
