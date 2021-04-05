873a5c575e87745d852749902ad6ae89f93cedf9
Pete Schwamb <pete@schwamb.net>
2021-04-05 15:14:58 -0700

    G6 plugin working

--- Scripts/copy-plugins.sh
+++ Scripts/copy-plugins.sh
@@ -11,28 +11,33 @@ shopt -s nullglob
 # Copy device plugins
 function copy_plugins {
     echo "Looking for plugins in $1"
-    for f in "$1"/*.loopplugin; do
+    for f in "$1"/*Plugin.xcframework; do
       plugin=$(basename "$f")
       echo Copying plugin: $plugin to frameworks directory in app
       plugin_path="$(readlink "$f" || echo "$f")"
-      plugin_as_framework_path="${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${plugin%.*}.framework"
-      rsync -va --exclude=Frameworks "$plugin_path/." "${plugin_as_framework_path}"
-      # Rename .plugin to .framework
+      platform_arch="${SWIFT_PLATFORM_TARGET_PREFIX}-${ARCHS_STANDARD// /_}${LLVM_TARGET_TRIPLE_SUFFIX}"
+      plugin_destination_path="${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${plugin%.*}.framework"
+      plugin_arch_platform_path="$plugin_path/${platform_arch}/${plugin%.*}.framework"
+      rsync -va --exclude=Frameworks "${plugin_arch_platform_path}/." "${plugin_destination_path}"
       if [ "$EXPANDED_CODE_SIGN_IDENTITY" != "-" ] && [ "$EXPANDED_CODE_SIGN_IDENTITY" != "" ]; then
         export CODESIGN_ALLOCATE=${DT_TOOLCHAIN_DIR}/usr/bin/codesign_allocate
         echo "Signing ${plugin} with ${EXPANDED_CODE_SIGN_IDENTITY_NAME}"
-        /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --timestamp=none --preserve-metadata=identifier,entitlements,flags "$plugin_as_framework_path"
+        /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --timestamp=none --preserve-metadata=identifier,entitlements,flags "$plugin_destination_path"
       else
         echo "Skipping signing, no identity set"
       fi
-      for framework_path in "${f}"/Frameworks/*.framework; do
-        framework=$(basename "$framework_path")
+      
+      dependencies=$(plutil -extract PluginDependencies xml1 -o - "${plugin_arch_platform_path}/Info.plist" | sed -n "s/.*<string>\(.*\)<\/string>.*/\1/p")
+      echo "Dependencies = ${dependencies}"
+      
+      for framework in ${dependencies}; do
+        framework_path="$1/${framework}.xcframework/${platform_arch}/${framework}.framework"
         echo "Copying plugin's framework $framework_path to ${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/."
         cp -avf "$framework_path" "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/."
         plugin_path="$(readlink "$f" || echo "$f")"
         if [ "$EXPANDED_CODE_SIGN_IDENTITY" != "-" ] && [ "$EXPANDED_CODE_SIGN_IDENTITY" != "" ]; then
           echo "Signing $framework for $plugin with $EXPANDED_CODE_SIGN_IDENTITY_NAME"
-          /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --timestamp=none --preserve-metadata=identifier,entitlements,flags "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${framework}"
+          /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --timestamp=none --preserve-metadata=identifier,entitlements,flags "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${framework}.framework"
         fi
       done
     done
@@ -40,14 +45,4 @@ function copy_plugins {
 
 copy_plugins "$BUILT_PRODUCTS_DIR"
 
-CARTHAGE_BUILD_DIR="${SRCROOT}/Carthage/Build"
-if [ -n "${IPHONEOS_DEPLOYMENT_TARGET}" ]; then
-CARTHAGE_BUILD_DIR="${CARTHAGE_BUILD_DIR}/iOS"
-elif [ -n "${WATCHOS_DEPLOYMENT_TARGET}" ]; then
-CARTHAGE_BUILD_DIR="${CARTHAGE_BUILD_DIR}/watchOS"
-else
-echo "ERROR: Unexpected deployment target type"
-exit 1
-fi
-
-copy_plugins "$CARTHAGE_BUILD_DIR"
+copy_plugins "${SRCROOT}/Carthage/Build"
