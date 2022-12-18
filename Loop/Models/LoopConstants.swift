8b4c404eec65344040815fc84f194791fc10e1d9
Pete Schwamb <pete@schwamb.net>
2022-12-18 14:47:57 -0600

    Update comment

--- Loop/Models/LoopConstants.swift
+++ Loop/Models/LoopConstants.swift
@@ -17,7 +17,7 @@ enum LoopConstants {
     
     static let maxCarbEntryQuantity = HKQuantity(unit: .gram(), doubleValue: 250) // cannot exceed this value
 
-    static let warningCarbEntryQuantity = HKQuantity(unit: .gram(), doubleValue: 99) // user is warned at this value
+    static let warningCarbEntryQuantity = HKQuantity(unit: .gram(), doubleValue: 99) // user is warned above this value
     
     static let validManualGlucoseEntryRange = HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 10)...HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 600)
     
