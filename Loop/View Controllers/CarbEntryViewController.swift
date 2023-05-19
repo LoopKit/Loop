b8a755e11fd24edab13e8f0dfc4bb4456ec21b4b
Marion Barker <marionbarker@earthlink.net>
2023-05-19 06:15:16 -0700

    Use maximumDateFutureInterval for Carb Entry
    The value for maximumDateFutureInterval is defined as 4 hours.
    But the usage in picker is hard-coded 1 hour.
    Use the defined value in the picker.

--- Loop/View Controllers/CarbEntryViewController.swift
+++ Loop/View Controllers/CarbEntryViewController.swift
@@ -405,7 +405,7 @@ final class CarbEntryViewController: LoopChartsTableViewController, Identifiable
                         cell.datePicker.preferredDatePickerStyle = .wheels
                     }
                 #endif
-                cell.datePicker.maximumDate = date.addingTimeInterval(.hours(1))
+                cell.datePicker.maximumDate = date.addingTimeInterval(.hours(maximumDateFutureInterval))
                 cell.datePicker.minimumDate = date.addingTimeInterval(.hours(-12))
                 cell.datePicker.minuteInterval = 1
                 cell.date = date
