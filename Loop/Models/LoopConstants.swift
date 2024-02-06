bbff919617d9cc17e5fddfcfbe3dfa558adca706
marionbarker <marionbarker@earthlink.net>
2024-02-05 17:14:25 -0800

    modify maxCarbEntryFutureTime to 4 hours

--- Loop/Models/LoopConstants.swift
+++ Loop/Models/LoopConstants.swift
@@ -25,7 +25,7 @@ enum LoopConstants {
     static let maxCarbAbsorptionTime = TimeInterval(hours: 8)
     
     static let maxCarbEntryPastTime = TimeInterval(hours: (-12))
-    static let maxCarbEntryFutureTime = TimeInterval(hours: 1)
+    static let maxCarbEntryFutureTime = TimeInterval(hours: 4)
 
     static let maxOverrideDurationTime = TimeInterval(hours: 24)
     
