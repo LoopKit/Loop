4a3ed0ee557dd1cb0c3f7593efcb0b0a487e74ea
bastiaan.verhaar <bastiaan.verhaar@scenius.nl>
2025-03-23 16:54:44 +0100

    fix: small configuration fixes

--- Loop/Views/LiveActivityBottomRowManagerView.swift
+++ Loop/Views/LiveActivityBottomRowManagerView.swift
@@ -12,17 +12,26 @@ import SwiftUI
 
 struct LiveActivityBottomRowManagerView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-
+    
     // The maximum items in the bottom row
     private let maxSize = 4
     
     @State var showAdd: Bool = false
-    @State var configuration: [BottomRowConfiguration] = (UserDefaults.standard.liveActivity ?? LiveActivitySettings()).bottomRowConfiguration
+    @State var configuration: [BottomRowConfiguration]
+    @State private var previousConfiguration: [BottomRowConfiguration]
+    @State private var isDirty = false
+    
+    init() {
+        configuration = (UserDefaults.standard.liveActivity ?? LiveActivitySettings()).bottomRowConfiguration
+        previousConfiguration = (UserDefaults.standard.liveActivity ?? LiveActivitySettings()).bottomRowConfiguration
+    }
     
     var addItem: ActionSheet {
         var buttons: [ActionSheet.Button] = BottomRowConfiguration.all.map { item in
             ActionSheet.Button.default(Text(item.description())) {
                 configuration.append(item)
+                
+                isDirty = configuration != previousConfiguration
             }
         }
         buttons.append(.cancel(Text(NSLocalizedString("Cancel", comment: "Button text to cancel"))))
@@ -37,6 +46,7 @@ struct LiveActivityBottomRowManagerView: View {
                     deleteButton
                         .onTapGesture {
                             onDelete(item.wrappedValue)
+                            isDirty = configuration != previousConfiguration
                         }
                     Text(item.wrappedValue.description())
                     
@@ -44,29 +54,34 @@ struct LiveActivityBottomRowManagerView: View {
                     editBars
                 }
             }
-                .onMove(perform: onReorder)
-                .deleteDisabled(true)
+            .onMove(perform: onReorder)
+            .deleteDisabled(true)
             
             Section {
                 Button(action: onSave) {
                     Text(NSLocalizedString("Save", comment: ""))
                 }
+                .disabled(!isDirty)
                 .buttonStyle(ActionButtonStyle())
                 .listRowInsets(EdgeInsets())
             }
         }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(
-                        action: { showAdd = true },
-                        label: { Image(systemName: "plus") }
-                    )
-                    .disabled(configuration.count >= self.maxSize)
-                }
+        .onAppear {
+            configuration = (UserDefaults.standard.liveActivity ?? LiveActivitySettings()).bottomRowConfiguration
+            previousConfiguration = (UserDefaults.standard.liveActivity ?? LiveActivitySettings()).bottomRowConfiguration
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(
+                    action: { showAdd = true },
+                    label: { Image(systemName: "plus") }
+                )
+                .disabled(configuration.count >= self.maxSize)
             }
-            .actionSheet(isPresented: $showAdd, content: { addItem })
-            .insetGroupedListStyle()
-            .navigationBarTitle(Text(NSLocalizedString("Bottom row", comment: "Live activity Bottom row configuration title")))
+        }
+        .actionSheet(isPresented: $showAdd, content: { addItem })
+        .insetGroupedListStyle()
+        .navigationBarTitle(Text(NSLocalizedString("Bottom row", comment: "Live activity Bottom row configuration title")))
     }
     
     @ViewBuilder
@@ -102,6 +117,7 @@ struct LiveActivityBottomRowManagerView: View {
     func onReorder(from: IndexSet, to: Int) {
         withAnimation {
             configuration.move(fromOffsets: from, toOffset: to)
+            isDirty = configuration != previousConfiguration
         }
     }
     
