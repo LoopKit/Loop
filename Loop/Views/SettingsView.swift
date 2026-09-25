06815d5b60f69be974a03a0a1f2e49ca259f5792
marionbarker <marionbarker@earthlink.net>
2026-09-25 11:56:33 -0700

    Add missing translations string, update WatchApp.xcscheme for Xcode 27

--- Loop/Views/SettingsView.swift
+++ Loop/Views/SettingsView.swift
@@ -365,8 +365,8 @@ extension SettingsView {
             LargeButton(action: { sheet = .favoriteFoods },
                         includeArrow: true,
                         imageView: Image("Favorite Foods Icon").renderingMode(.template).foregroundColor(carbTintColor),
-                        label: "Favorite Foods",
-                        descriptiveText: "Simplify Carb Entry")
+                        label: NSLocalizedString("Favorite Foods", comment: "Label for Favorite Foods icon in Settings"),
+                        descriptiveText: NSLocalizedString("Simplify Carb Entry", comment: "Subheading for Favorite Foods in Settings"))
         }
     }
     
